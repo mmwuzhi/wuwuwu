110a21f17b72baf10aeca846756789f0ff854e29
mmwuzhi <mmwuzhi@gmail.com>
2022-04-17 11:07:23 +0900

    feat: new next app

---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>ううう</title>
+      </Head>
+
+      <main>
+        <h1>Welcome to wuwuwu.cc</h1>
+      </main>
+
+      <footer>wuwuwu.cc</footer>
+    </div>
+  )
+}
+
+export default Home
